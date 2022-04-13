95fb9ed1722b1b1b63f716f8b7379280db7404ac
sanyath007 <sanyath007@gmail.com>
2022-04-13 16:39:07 +0700

    Create new Material, MaterialCategory models and MaterialController

--- app/Models/MaterialCategory.php
+++ app/Models/MaterialCategory.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AssetCategory extends Model
+class MaterialCategory extends Model
 {
-    protected $table = 'asset_categories';
+    protected $table = 'material_categories';
     // protected $primaryKey = 'type_id';
     // public $incrementing = false; // false = ไม่ใช้ options auto increment
     // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
