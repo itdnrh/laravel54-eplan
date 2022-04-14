16fc159000f430c9753dfdca218427d2d304a465
sanyath007 <sanyath007@gmail.com>
2022-04-14 17:56:05 +0700

    Rename of ServiceType and ConstructType models to ServiceCategory and ConstructCategory respectively

--- app/Models/ConstructCategory.php
+++ app/Models/ConstructCategory.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ConstructType extends Model
+class ConstructCategory extends Model
 {
-    protected $table = 'construct_types';
+    protected $table = 'construct_categories';
     // protected $primaryKey = 'type_id';
     // public $incrementing = false; // false = ไม่ใช้ options auto increment
     // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
--- app/Models/ServiceCategory.php
+++ app/Models/ServiceCategory.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ServiceType extends Model
+class ServiceCategory extends Model
 {
-    protected $table = 'service_types';
+    protected $table = 'service_categories';
     // protected $primaryKey = 'type_id';
     // public $incrementing = false; // false = ไม่ใช้ options auto increment
     // public $timestamps = false; // false = ไม่ใช้ field updated_at และ created_at
