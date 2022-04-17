bd2c1a279d74195c590351ed1066a163c6c29755
sanyath007 <sanyath007@gmail.com>
2022-04-18 00:07:55 +0700

    Modify planConstructCtrl by removing unused or duplicated functions and models and modeify getAll function with adding query string to api route path

--- public/js/controllers/planConstructCtrl.js
+++ public/js/controllers/planConstructCtrl.js
@@ -1,42 +1,8 @@
 app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, StringFormatService, PaginateService) {
 /** ################################################################################## */
     $scope.loading = false;
-    $scope.cboYear = parseInt(moment().format('MM')) > 9
-                        ? (moment().year() + 544).toString()
-                        : (moment().year() + 543).toString();
-    $scope.cboMonth = moment().format('MM');
-    $scope.cboCategory = "";
-    $scope.cboDepart = "";
-    $scope.cboStatus = "";
-    $scope.cboMenu = "";
-    $scope.searchKeyword = "";
-    $scope.cboQuery = "";
-    $scope.budgetYearRange = [2560,2561,2562,2563,2564,2565,2566,2567];
-    $scope.monthLists = [
-        { id: '01', name: 'มกราคม' },
-        { id: '02', name: 'กุมภาพันธ์' },
-        { id: '03', name: 'มีนาคม' },
-        { id: '04', name: 'เมษายน' },
-        { id: '05', name: 'พฤษภาคม' },
-        { id: '06', name: 'มิถุนายน' },
-        { id: '07', name: 'กรกฎาคม' },
-        { id: '08', name: 'สิงหาคม' },
-        { id: '09', name: 'กันยายน' },
-        { id: '10', name: 'ตุลาคม' },
-        { id: '11', name: 'พฤศจิกายน' },
-        { id: '12', name: 'ธันวาคม' },
-    ];
-
     $scope.constructs = [];
     $scope.pager = [];
-
-    $scope.forms = {
-        depart: [],
-        division: [],
-    };
-
-    let tmpDeparts = [];
-    let tmpDivisions = [];
 
     $scope.construct = {
         construct_id: '',
@@ -47,6 +13,7 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
         division_id: '',
         category_id: '',
         group_id: '',
+        item_id: '',
         desc: '',
         location: '',
         building_id: '',
@@ -81,29 +48,6 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
         //     });
         // });
 
-    $('#sent_date')
-        .datepicker(dtpOptions)
-        .datepicker('update', new Date());
-
-    $('#po_date')
-        .datepicker(dtpOptions)
-        .datepicker('update', new Date());
-
-    $scope.initForms = (data) => {
-        if (data) {
-            tmpDeparts = data.departs ? data.departs : [];
-            tmpDivisions = data.divisions ? data.divisions : [];
-        }
-    };
-
-    $scope.onFactionSelected = function(faction) {
-        $scope.forms.departs = tmpDeparts.filter(dep => dep.faction_id == faction);
-    };
-
-    $scope.onDepartSelected = function(depart) {
-        $scope.forms.divisions = tmpDivisions.filter(div => div.depart_id == depart);
-    };
-
     $scope.clearConstructObj = function() {
         $scope.construct = {
             construct_id: '',
@@ -114,6 +58,7 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
             division_id: '',
             category_id: '',
             group_id: '',
+            item_id: '',
             desc: '',
             location: '',
             building_id: '',
@@ -141,14 +86,13 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
         $scope.constructs = [];
         $scope.loading = true;
 
-        let year    = $scope.cboYear === '' ? 0 : $scope.cboYear;
-        let cate    = $scope.cboCategory === '' ? 0 : $scope.cboCategory;
-        let depart  = $scope.cboDepart === '' ? 0 : $scope.cboDepart;
-        let status  = $scope.cboStatus === '' ? '-' : $scope.cboStatus;
-        let menu    = $scope.cboMenu === '' ? 0 : $scope.cboMenu;
-        let query   = $scope.cboQuery === '' ? '' : `?${$scope.cboQuery}`;
+        let year    = $scope.cboYear === '' ? '' : $scope.cboYear;
+        let cate    = $scope.cboCategory === '' ? '' : $scope.cboCategory;
+        let depart  = $scope.cboDepart === '' ? '' : $scope.cboDepart;
+        let status  = $scope.cboStatus === '' ? '' : $scope.cboStatus;
+        let menu    = $scope.cboMenu === '' ? '' : $scope.cboMenu;
 
-        $http.get(`${CONFIG.baseUrl}/constructs/search?depart=${depart}`)
+        $http.get(`${CONFIG.baseUrl}/plans/search?type=1&year=${year}&cate=${cate}&status=${status}&depart=${depart}&menu=${menu}`)
         .then(function(res) {
             $scope.setConstructs(res);
 
@@ -160,7 +104,7 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
     };
 
     $scope.setConstructs = function(res) {
-        const { data, ...pager } = res.data.constructs;
+        const { data, ...pager } = res.data.plans;
 
         $scope.constructs = data;
         $scope.pager = pager;
@@ -215,56 +159,6 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
         // $scope.service.service_date     = StringFormatService.convFromDbDate(data.plan.service.service_date);
     };
 
-    $scope.showSupportedForm = function() {
-        $('#supported-from').modal('show');
-    };
-
-    $scope.sendSupportedDoc = (e) => {
-        e.preventDefault();
-
-        let data = {
-            doc_no: $('#doc_no').val(),
-            doc_date: $('#doc_date').val(),
-            sent_date: $('#sent_date').val(),
-            sent_user: $('#sent_user').val(),
-        };
-
-        $http.post(`${CONFIG.baseUrl}/plans/send-supported/${$scope.service.service_id}`, data)
-        .then(function(res) {
-            console.log(res.data);
-        }, function(err) {
-            console.log(err);
-        });
-
-        /** Redirect to list view */
-        window.location.href = `${CONFIG.baseUrl}/services/list`;
-    };
-
-    $scope.showPoForm = function() {
-        $('#po-form').modal('show');
-    };
-
-    $scope.createPO = (e) => {
-        e.preventDefault();
-
-        let data = {
-            po_no: $('#po_no').val(),
-            po_date: $('#po_date').val(),
-            po_net_total: $('#po_net_total').val(),
-            po_user: $('#po_user').val(),
-        };
-
-        $http.post(`${CONFIG.baseUrl}/plans/create-po/${$scope.service.service_id}`, data)
-        .then(function(res) {
-            console.log(res.data);
-        }, function(err) {
-            console.log(err);
-        });
-
-        /** Redirect to list view */
-        window.location.href = `${CONFIG.baseUrl}/services/list`;
-    };
-
     $scope.store = function(event, form) {
         event.preventDefault();
 
@@ -292,15 +186,5 @@ app.controller('planConstructCtrl', function(CONFIG, $scope, $http, toaster, Str
         if(confirm(`คุณต้องลบใบลาเลขที่ ${id} ใช่หรือไม่?`)) {
             $('#frmDelete').submit();
         }
-    };
-
-    $scope.approval = null;
-    $scope.showApprovalDetail = function(id) {
-        $scope.getById(id, function(data) {
-            console.log(data);
-            $scope.approval = data.leave;
-        });
-
-        $('#approval-detail').modal('show');
     };
 });
