15533077fc5e22ab16fa51ead99e2918d85634d8
sanyath007 <sanyath007@gmail.com>
2022-04-22 13:09:11 +0700

    Re-design supports/timeline view

--- resources/views/supports/timeline.blade.php
+++ resources/views/supports/timeline.blade.php
@@ -16,90 +16,196 @@
     </section>
 
     <!-- Main content -->
-    <section class="content" ng-controller="supportCtrl">
+    <section class="content" ng-controller="supportCtrl" ng-init="getAll()">
 
         <div class="row">
             <div class="col-md-12">
 
                 <div class="box box-primary">
-                    <div class="box-header">
+                    <div class="box-header with-border">
                         <h3 class="box-title">ติดตามพัสดุ</h3>
                     </div><!-- /.box-header -->
                     <div class="box-body">
-                        <ul class="timeline timeline-inverse">
-                            <li class="time-label">
-                                <span class="bg-red">
-                                    10 Feb. 2014
-                                </span>
-                            </li>
+                        <input
+                            type="hidden"
+                            id="user"
+                            name="user"
+                            value="{{ Auth::user()->person_id }}"
+                        />
+                        <input
+                            type="hidden"
+                            id="depart"
+                            name="depart"
+                            value="{{ Auth::user()->memberOf->depart_id }}"
+                        />
 
-                            <li>
-                                <i class="fa fa-envelope bg-blue"></i>
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-                                    <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
-                                    <div class="timeline-body">
-                                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                                        weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                                        jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                                        quora plaxo ideeli hulu weebly balihoo...
-                                    </div>
-                                    <div class="timeline-footer">
-                                        <a class="btn btn-primary btn-xs">Read more</a>
-                                        <a class="btn btn-danger btn-xs">Delete</a>
-                                    </div>
+                        <div class="row">
+                            <div class="col-md-8">
+                                <table class="table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 4%; text-align: center;">#</th>
+                                            <th style="width: 30%;">บันทึกขอสนับสนุน</th>
+                                            <th>รายการ</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr ng-repeat="(index, support) in supports">
+                                            <td style="text-align: center;">@{{ index+1 }}</td>
+                                            <td>
+                                                <p style="margin: 2px;">เลขที่ @{{ support.doc_no }}</p>
+                                                <p style="margin: 2px;">ลวท. @{{ support.doc_date | thdate }}</p>
+                                            </td>
+                                            <td>
+                                                <ul style="margin: 0 5px; padding: 0 10px;">
+                                                    <li ng-repeat="(index, detail) in support.details" style="margin: 5px 0;">
+                                                        <span>@{{ detail.plan.plan_no }}</span>
+                                                        <span>@{{ detail.plan.plan_item.item.item_name }} จำนวน </span>
+                                                        <span>@{{ detail.plan.plan_item.amount | currency:'':0 }}</span>
+                                                        <span>@{{ detail.plan.plan_item.unit.name }}</span>
+                                                        <a href="#" class="text-aqua">
+                                                            <i class="fa fa-eye" aria-hidden="true"></i>
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col-md-4">
+                                <div>
+                                    <h4 style="margin: 0;">เลขที่แผน ...</h4>
+                                    <p style="margin: 5px 0;">รายการ ... จำนวน...</p>
                                 </div>
-                            </li>
+                                <ul class="timeline timeline-inverse">
+                                    <li class="time-label">
+                                        <span class="bg-red">
+                                            01/04/2565
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-envelope bg-blue"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                ส่งบันทึกขอสนับสนุนแล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                            <div class="timeline-footer">
+                                                <a class="btn btn-primary btn-xs">Read more</a>
+                                                <a class="btn btn-danger btn-xs">Delete</a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-pencil-square-o bg-yellow"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                รับเอกสารขอสนับสนุนแล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                        </div>
+                                    </li>
 
-                            <li>
-                                <i class="fa fa-user bg-aqua"></i>
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
-                                    <h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request
-                                    </h3>
-                                </div>
-                            </li>
+                                    <li class="time-label">
+                                        <span class="bg-red">
+                                            05/04/2565
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-cart-plus bg-purple"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                ออกใบสั่งซื้อแล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                        </div>
+                                    </li>
 
-                            <li>
-                                <i class="fa fa-comments bg-yellow"></i>
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
-                                    <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
-                                    <div class="timeline-body">
-                                        Take me to your leader!
-                                        Switzerland is small and neutral!
-                                        We are more like Germany, ambitious and misunderstood!
-                                    </div>
-                                    <div class="timeline-footer">
-                                        <a class="btn btn-warning btn-flat btn-xs">View comment</a>
-                                    </div>
-                                </div>
-                            </li>
+                                    <li class="time-label">
+                                        <span class="bg-red">
+                                            20/04/2565
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-check-square-o bg-green"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                ตรวจรับแล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                        </div>
+                                    </li>
 
-                            <li class="time-label">
-                                <span class="bg-green">
-                                    3 Jan. 2014
-                                </span>
-                            </li>
+                                    <li class="time-label">
+                                        <span class="bg-red">
+                                            22/04/2565
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-paper-plane bg-aqua"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                ส่งเบิกเงินแล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                        </div>
+                                    </li>
 
-                            <li>
-                                <i class="fa fa-camera bg-purple"></i>
-                                <div class="timeline-item">
-                                    <span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
-                                    <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-                                    <div class="timeline-body">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                        <img src="http://placehold.it/150x100" alt="..." class="margin">
-                                    </div>
-                                </div>
-                            </li>
+                                    <li class="time-label">
+                                        <span class="bg-red">
+                                            ..../......./.....
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-university bg-maroon"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                ตั้งหนี้แล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                        </div>
+                                    </li>
 
-                            <li>
-                                <i class="fa fa-clock-o bg-gray"></i>
-                            </li>
-                        </ul>
+                                    <li class="time-label">
+                                        <span class="bg-red">
+                                            ..../......./.....
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <i class="fa fa-money bg-green"></i>
+                                        <div class="timeline-item">
+                                            <h3 class="timeline-header">
+                                                เบิกจ่ายแล้ว
+                                            </h3>
+                                            <div class="timeline-body">
+                                                ...
+                                            </div>
+                                            <div class="timeline-footer">
+                                                <a class="btn btn-primary btn-xs">Read more</a>
+                                                <a class="btn btn-danger btn-xs">Delete</a>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <i class="fa fa-clock-o bg-gray"></i>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div><!-- /.box-body -->
 
                     <!-- Loading (remove the following to stop the loading)-->
