92e34c6edfad29f9abc4b4ec045114d9d75e5d86
sanyath007 <sanyath007@gmail.com>
2022-06-17 10:10:42 +0700

    Add box component for containing partial views in projects/detail view

--- resources/views/projects/detail.blade.php
+++ resources/views/projects/detail.blade.php
@@ -181,32 +181,41 @@
                                     ></textarea>
                                 </div>
                             </div>
+
                             <!-- ================ Timeline Section ================ -->
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div style="padding: 0 20px;">
-                                        <h4>Timeline</h4>
+                            <div class="box">
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div style="padding: 0 20px;">
+                                                <h4>Timeline</h4>
 
-                                        @include('projects._timelines-list')
+                                                @include('projects._timelines-list')
 
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div><br />
 
                             <!-- ================ Payment Section ================ -->
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div style="padding: 0 20px;">
-                                        <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
-                                            <h4 style="margin: 0;">การเบิกจ่าย</h4>
-                                            <a href="#" class="btn btn-primary" ng-click="showPaymentForm()">
-                                                เพิ่มรายการเบิกจ่าย
-                                            </a>
+                            <div class="box">
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div style="padding: 0 20px;">
+                                                <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
+                                                    <h4 style="margin: 0;">การเบิกจ่าย</h4>
+                                                    <a href="#" class="btn btn-primary" ng-click="showPaymentForm()">
+                                                        เพิ่มรายการเบิกจ่าย
+                                                    </a>
+                                                </div>
+        
+                                                @include('projects._payment-list')
+                                                @include('projects._payment-form')
+        
+                                            </div>
                                         </div>
-
-                                        @include('projects._payment-list')
-                                        @include('projects._payment-form')
-
                                     </div>
                                 </div>
                             </div>
