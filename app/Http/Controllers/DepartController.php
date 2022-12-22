e3ef3cf331a57f11fb06a706ab6f655505036bb6
sanyath007 <sanyath007@gmail.com>
2022-12-22 10:26:24 +0700

    Add index and search methods to DepartController and create 2 new departCtrl and divisionCtrl and all departs view

--- app/Http/Controllers/DepartController.php
+++ app/Http/Controllers/DepartController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Depart;
 
 class DepartController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('departs.list', [
+
+        ]);
+    }
+
+    public function search()
+    {
+        $departs = Depart::all();
+
+        return [
+            "departs" => $departs
+        ];
+    }
 }
