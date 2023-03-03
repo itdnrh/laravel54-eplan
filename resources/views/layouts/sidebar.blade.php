72b7a379305608674b7c2d0a74cb7dfd8c16fd7b
sanyath007 <sanyath007@gmail.com>
2023-03-03 11:37:44 +0700

    Grant privileges of purchase ordering to depart of strategy and plan in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -185,7 +185,7 @@
 					@if (
 						Auth::user()->person_id == '1300200009261' ||
 						Auth::user()->memberOf->depart_id == 2 ||
-						count(Auth::user()->delegations) > 0
+						Auth::user()->memberOf->depart_id == 4
 					)
 						<li class="treeview" ng-class="{ 'menu-open active': menu == 'orders' }">
 							<a href="#">
